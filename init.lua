6050eb7a8821bb2c3f19aaad7bdab277c68dbd31
Simanraj Sadana <simanraj1123@gmail.com>
2024-01-28 17:29:20 +0100

    Added custom autocmds for python files.

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require('configs/options')
 require('configs/lazy')
+require('configs/options')
 require('configs/keymaps')
+require('configs/autocmds')
---
+++ lua/configs/autocmds.lua
@@ -0,0 +1,22 @@
+-- PYTHON 
+-- Keymap shortcut and options
+local opts = { noremap = true, silent = true }
+local km = vim.api.nvim_buf_set_keymap
+
+-- Creating a augroup
+local group = vim.api.nvim_create_augroup("Python_AutoCmds", { clear = true })
+
+-- Creating the AutoCmds in the Python_AutoCmds augroup
+vim.api.nvim_create_autocmd("FileType", {
+	group = group,
+	pattern = "python",
+	callback = function()
+		local current_buf = vim.api.nvim_get_current_buf()
+		km(current_buf, "n", "<leader>cx", ":w | !python3 % <CR>", opts)
+		km(current_buf, "n", "<leader>c/", "0i# <esc>", opts)
+		km(current_buf, "n", "<leader>c.", "0xx", opts)
+		km(current_buf, "v", "<leader>c/", "<C-v>0I# <esc> '>", opts)
+		km(current_buf, "v", "<leader>c.", [[<esc>:'<,'>s/^#\s\?// | :let @/=''<CR> \'>]], opts)
+		km(current_buf, "n", "<leader>m", "Iprint(<esc>A)<esc>", opts)
+	end,
+})
