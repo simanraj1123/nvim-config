3e698735a0f58460458972ab62d4f7565da1a3b5
Simanraj Sadana <simanraj1123@gmail.com>
2024-01-28 23:34:54 +0100

    Adding lsp support for rust (rust-analyzer)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright" },
+				ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
 			})
 		end,
 	},
@@ -26,6 +26,9 @@ return {
 				capabilities = capabilities,
 			})
 			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
 			})
 
