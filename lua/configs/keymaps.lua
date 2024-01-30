09e004c1af2b208938a364c800a4a53441540248
Simanraj Sadana <s.sadana@parityqc.com>
2024-01-30 08:39:26 +0100

    Added keymaps for buffer management commands

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -4,3 +4,8 @@ local opts = { noremap = true, silent = true }
 
 -- Map windows commands, i.e., <C-w> commands to <leader>w
 km("n", "<leader>w", "<C-w>", opts)
+
+-- Buffer commands
+km("n", "<leader>bb", ":ls<cr>", opts)
+km("n", "<leader>bn", ":bn<cr>", opts)
+km("n", "<leader>bd", ":bd<cr>", opts)
