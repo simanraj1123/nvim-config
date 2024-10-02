8c33db29865a14aa9f35f8c6576cbaaefd6fc004
Simanraj Sadana <simanrajsadana@gmail.com>
2024-10-03 01:12:32 +0200

    UI select is added that provides a UI for code actions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,4 +5,29 @@ return {
         -- or                              , branch = '0.1.x',
         dependencies = { "nvim-lua/plenary.nvim" },
     },
+
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+
+
+        -- This is your opts table
+        config = function()
+            require("telescope").setup ({
+              extensions = {
+                ["ui-select"] = {
+                  require("telescope.themes").get_dropdown {
+                    -- even more opts
+                  }
+                }
+              }
+            })
+            -- To get ui-select loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require("telescope").load_extension("ui-select")
+        end
+
+
+
+
+    },
 }
