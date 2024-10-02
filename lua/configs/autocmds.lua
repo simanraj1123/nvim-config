9b2672afa47a2e68192beaf5433f8987dafa9345
Simanraj Sadana <simanrajsadana@gmail.com>
2024-10-03 01:12:50 +0200

    Autocommands for rust language is added.

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -20,3 +20,23 @@ vim.api.nvim_create_autocmd("FileType", {
 		km(current_buf, "n", "<leader>m", "Iprint(<esc>A)<esc>", opts)
 	end,
 })
+
+
+
+-- Creating a augroup
+local group = vim.api.nvim_create_augroup("Rust_AutoCmds", { clear = true })
+
+-- Creating the AutoCmds in the Python_AutoCmds augroup
+vim.api.nvim_create_autocmd("FileType", {
+	group = group,
+	pattern = "rust",
+	callback = function()
+		local current_buf = vim.api.nvim_get_current_buf()
+		km(current_buf, "n", "<leader>cx", ":w | !rustc % -o %:r && ./%:r <CR>", opts)
+		km(current_buf, "n", "<leader>c/", "0i// <esc>", opts)
+		km(current_buf, "n", "<leader>c.", "0xxx", opts)
+		km(current_buf, "v", "<leader>c/", "<C-v>0I// <esc> '>", opts)
+		km(current_buf, "v", "<leader>c.", [[<esc>:'<,'>s/^\/\/\s\?// | :let @/=''<CR> \'>]], opts)
+		km(current_buf, "n", "<leader>m", "Iprint(<esc>A)<esc>", opts)
+	end,
+})
